9e1018fa0f56507a7e054f8188b0975e7ca3cf8a
Matheus <mcardoso@exactsales.com.br>
2022-11-29 21:53:25 -0300

    Try run

--- functions/server.js
+++ functions/server.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 const app = express()
-import serverRoutes from './routes/serveRoutes.js';
 import serverless from 'serverless-http'
+import serverRoutes from '../routes/serveRoutes.js';
+
 
 app.use(cors({
   origin: 'http://localhost:3000'
@@ -14,10 +15,12 @@ app.use(serverRoutes)
 // let listener = app.listen(process.env.PORT || 3030, () =>{
 //   console.log(`Server is running ${listener.address().port}`);
 // })
-let handler = serverless(app);
-export default handler(async (event, context) => {
-  const result = await handler(event, context);
-  console.log(event);
-  console.log(context);
-  return result;
-}) 
+// let handler = serverless(app);
+// export default handler(async (event, context) => {
+//   const result = await handler(event, context);
+//   console.log(event);
+//   console.log(context);
+//   return result;
+// }) 
+
+module.exports.handler = serverless(app);
